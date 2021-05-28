667dbc31d907cbcc96c8cacbe42239a77d5bd7a8
chanlakSou <chanlak@example.com>
2021-05-28 13:51:39 +0700

    First Commit Laravel

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,3 +11,24 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for a given user.
+     *
+     * @param  int  $id
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        return view('user.profile', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
+}
